a5792f125d4ea28afa5dc67213e9a12b9ad3c3c1
erik-rose <roseeri5@mail.msu.edu>
2020-02-05 12:48:21 -0500

    logo animation v1

--- jquery.scripts.js
+++ jquery.scripts.js
@@ -1,21 +1,18 @@
 var inArea,
     map = $('#logomap'),
     captions = {
-        reviews: ["Paul McCartney - Bass Guitar and Vocals",
-            "Paul McCartney's song, Yesterday, recently voted the most popular song "
-                + "of the century by a BBC poll, was initially composed without lyrics. "
-                + "Paul used the working title 'scrambled eggs' before coming up with the final words."],
-        jobs: ["Ringo Starr - Drums",
-            "Dear Prudence was written by John and Paul about Mia Farrow's sister, Prudence, "
-            + "when she wouldn't come out and play with Mia and the Beatles at a religious retreat "
-            + "in India."],
-        journals: ["John Lennon - Guitar and Vocals",
-            "In 1962, The Beatles won the Mersyside Newspaper's biggest band in Liverpool "
-            + "contest principally because they called in posing as different people and voted "
-            + "for themselves numerous times."],
-        books: ["George Harrison - Lead Guitar and Vocals",
-            "The Beatles' last public concert was held in San Francisco's Candlestick "
-            + "Park on August 29, 1966."]
+        reviews: ["Reviews",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. "],
+        jobs: ["Job Guide",
+            "Habitasse platea dictumst quisque sagittis purus sit amet volutpat. Amet risus nullam eget felis eget. Elit duis tristique sollicitudin nibh sit amet commodo nulla facilisi. Ut aliquam purus sit amet luctus. Sed viverra ipsum nunc aliquet bibendum enim facilisis gravida neque. Sed lectus vestibulum mattis ullamcorper velit. Vitae proin sagittis."],
+        journals: ["Journals",
+            "Velit scelerisque in dictum non. Porttitor massa id neque aliquam vestibulum morbi blandit cursus risus. Tristique et egestas quis ipsum suspendisse ultrices. Turpis egestas integer eget aliquet nibh praesent tristique magna. Fames ac turpis egestas maecenas pharetra convallis posuere morbi leo. Suspendisse in est ante in. Blandit volutpat maecenas volutpat blandit aliquam. Donec massa sapien faucibus et molestie ac feugiat sed lectus. Rutrum quisque non tellus orci ac auctor augue. Eget sit amet tellus cras adipiscing enim."],
+        books: ["Book Channel",
+            "Ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan. Suspendisse sed nisi lacus sed viverra tellus in hac. Magna sit amet purus gravida quis blandit turpis. Aliquam etiam erat velit scelerisque. Orci phasellus egestas tellus rutrum tellus pellentesque. Donec massa sapien faucibus et molestie ac feugiat sed. Tempor orci eu lobortis elementum nibh tellus. Ac auctor augue mauris augue neque. Sit amet risus nullam eget felis eget nunc. Nisi est sit amet facilisis. "],
+        networks: ["Networks",
+            "Egestas integer eget aliquet nibh praesent tristique magna sit amet. Sagittis purus sit amet volutpat consequat mauris nunc congue. Sollicitudin tempor id eu nisl nunc mi. Tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Posuere morbi leo urna molestie at elementum eu facilisis. Semper quis lectus nulla at volutpat diam ut venenatis tellus. Lorem mollis aliquam ut porttitor leo a diam. Elementum nisi quis eleifend quam adipiscing vitae proin sagittis. "],
+         announcements: ["Announcements",
+            "Viverra vitae congue eu consequat ac felis donec. Ornare aenean euismod elementum nisi quis eleifend quam adipiscing. Cursus turpis massa tincidunt dui ut. Lacus suspendisse faucibus interdum posuere. Turpis nunc eget lorem dolor. Morbi tincidunt ornare massa eget egestas. Varius quam quisque id diam vel quam elementum. Nunc sed velit dignissim sodales ut eu sem integer vitae. Viverra mauris in aliquam sem fringilla ut morbi."],    
     },
     single_opts = {
         fillColor: '000000',
